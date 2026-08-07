8c442808f4da3578542d42f9ca7ca943cb97ca40
MikeCat2008 <samuelgargri@gmail.com>
2026-08-07 13:30:41 +0200

    feat: rewrite and improve GUI, add i18n, and add version label

--- src/gui.py
+++ src/gui.py
@@ -14,10 +14,12 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
+import gui_locales
 import main
 
 import tkinter as tk
 from tkinter import ttk, filedialog
+
 import webbrowser
 
 from pathlib import Path
@@ -28,7 +30,154 @@ class ExporterApp:
         self.root.title("Smartboard Notebook Exporter")
         self.root.configure(bg="#f5f5f5")
 
-        self.extension_map = {
+        # --- VARIABLES ---
+        ## --- i18n ---
+        self.var_lang = "en" # Set default language: "en","es"
+        ## --- main ---
+        self.var_nbfile = tk.StringVar(value="")
+        self.var_expfile_outdir = tk.StringVar(value="")
+        self.var_expfile_name = tk.StringVar(value="")
+        self.var_expfile_outtype = tk.StringVar()
+        self.var_expfile_outtypeext = tk.BooleanVar(value=True)
+        ## --- Name Preview ---
+        ## Update variables whenever there's something new to update name preview
+        self.var_expfile_nameprev = None
+        self.var_nbfile.trace_add("write", self.expfile_nameprev_update)
+        self.var_expfile_name.trace_add("write", self.expfile_nameprev_update)
+        self.var_expfile_outtype.trace_add("write", self.expfile_nameprev_update)
+        self.var_expfile_outtypeext.trace_add("write", self.expfile_nameprev_update)
+        ## --- Export Status ---
+        self.var_export_active = tk.BooleanVar(value=False)
+
+        # --- VISUAL ELEMENTS LAYOUT ---
+        self.main_frame = tk.Frame(root, bg="#f5f5f5", padx=20, pady=20)
+        self.main_frame.pack()
+
+        ## --- App Name ---
+        tk.Label(self.main_frame, text="Smartboard notebook Exporter", font=("Arial", 20, "bold"), bg="#f5f5f5", fg="#333").grid(row=0, column=0, columnspan=4, pady=(0, 10))
+
+        ttk.Separator(self.main_frame, orient='horizontal').grid(row=1, column=0, columnspan=4, sticky="ew", pady=(0, 10))
+
+        ## --- Version Section ---
+        self.lbl_ver = tk.Label(self.main_frame, font=("Arial", 12, "bold"), bg="#f5f5f5")
+        self.lbl_ver.grid(row=2, column=0, columnspan=2, sticky="w", padx=(0,10))
+
+        self.lbl_ver_curtxt = tk.Label(self.main_frame, font=("Arial", 10), bg="#f5f5f5")
+        self.lbl_ver_curtxt.grid(row=3, column=0, sticky="w")
+
+        self.lbl_ver_cur = tk.Label(self.main_frame, text="v1.1.0", font=("Arial", 10, "bold"), bg="#f5f5f5")
+        self.lbl_ver_cur.grid(row=3, column=1, sticky="e", padx=(0,10))
+
+        self.btn_ver_chkupd = tk.Button(self.main_frame, command=lambda: webbrowser.open_new("https://github.com/MikeCat2008/smartboard-notebook-exporter/releases/latest"))
+        self.btn_ver_chkupd.grid(row=4, column=0, columnspan=2, sticky="ew", padx=(0,10))
+
+        ## --- i18n Section ---
+        self.lbl_lang = tk.Label(self.main_frame, font=("Arial", 12, "bold"), bg="#f5f5f5")
+        self.lbl_lang.grid(row=2, column=2, columnspan=2, sticky="w")
+
+        self.lbl_lang_info = tk.Label(self.main_frame, font=("Arial", 10), bg="#f5f5f5")
+        self.lbl_lang_info.grid(row=3, column=2, sticky="w")
+
+        self.frame_lang = tk.Frame(self.main_frame, bg="#f5f5f5")
+        self.frame_lang.grid(row=4, rowspan=2, column=2, columnspan=2, sticky="w")
+
+        self.btn_lang_es = tk.Button(self.frame_lang, text="ES", width="3", command=lambda: self.set_lang("es"))
+        self.btn_lang_es.grid(row=1, column=0, sticky="w", padx=(0, 5))
+
+        self.btn_lang_en = tk.Button(self.frame_lang, text="EN", width="3", command=lambda: self.set_lang("en"))
+        self.btn_lang_en.grid(row=1, column=1, sticky="w", padx=(0, 5))
+
+        ttk.Separator(self.main_frame, orient='horizontal').grid(row=6, column=0, columnspan=4, sticky="ew", pady=(10, 10))
+
+        ## --- Notebook File Section ---
+        self.lbl_nbfile = tk.Label(self.main_frame, font=("Arial", 14, "bold"), bg="#f5f5f5")
+        self.lbl_nbfile.grid(row=7, column=0, sticky="w")
+
+        self.ent_nbfile = tk.Entry(self.main_frame, textvariable=self.var_nbfile, width=40)
+        self.ent_nbfile.grid(row=8, column=0, columnspan=3, sticky="ew", padx=(0, 10), pady=10)
+
+        self.btn_nbfile_browse = tk.Button(self.main_frame, command=self.nbfile_search)
+        self.btn_nbfile_browse.grid(row=8, column=3, sticky="ew")
+
+        ## --- Export File Section ---
+        self.lbl_expfile = tk.Label(self.main_frame, font=("Arial", 14, "bold"), bg="#f5f5f5")
+        self.lbl_expfile.grid(row=9, column=0, sticky="w", pady=(20, 5))
+
+        ### Output Directory
+        self.lbl_expfile_outdir = tk.Label(self.main_frame, font=("Arial", 10), bg="#f5f5f5")
+        self.lbl_expfile_outdir.grid(row=10, column=0, sticky="w")
+
+        self.ent_expfile_outdir = tk.Entry(self.main_frame, textvariable=self.var_expfile_outdir)
+        self.ent_expfile_outdir.grid(row=11, column=0, columnspan=3, sticky="ew", padx=(0, 10), pady=5)
+
+        self.btn_expfile_outdir = tk.Button(self.main_frame, command=self.expfile_outfold_search)
+        self.btn_expfile_outdir.grid(row=11, column=3, sticky="ew")
+
+        ### Custom Name
+        self.lbl_expfile_name = tk.Label(self.main_frame, font=("Arial", 10), bg="#f5f5f5")
+        self.lbl_expfile_name.grid(row=12, column=0, sticky="w", pady=(10, 0))
+
+        self.ent_expfile_name = tk.Entry(self.main_frame, textvariable=self.var_expfile_name)
+        self.ent_expfile_name.grid(row=13, column=0, sticky="ew", padx=(0, 5))
+
+        ### Output Type
+        self.lbl_expfile_outtype = tk.Label(self.main_frame, font=("Arial", 10), bg="#f5f5f5")
+        self.lbl_expfile_outtype.grid(row=12, column=1, sticky="w", pady=(10, 0))
+
+        self.combo_expfile_outtype = ttk.Combobox(self.main_frame, state="readonly", textvariable=self.var_expfile_outtype, values=['svg-fixed-pages', 'png-pages', 'pdf-svg-pages', 'pdf-png-pages', 'pdf-svg-merged', 'pdf-png-merged'])
+        self.combo_expfile_outtype.current(4)
+        self.combo_expfile_outtype.grid(row=13, column=1, columnspan=2, sticky="ew", padx=5)
+
+        ### Output Type Extension Label
+        self.chkbtn_expfile_outtypeext = tk.Checkbutton(self.main_frame, variable=self.var_expfile_outtypeext, bg="#f5f5f5")
+        self.chkbtn_expfile_outtypeext.grid(row=13, column=3)
+
+        ### Name Preview
+        self.lbl_expfile_nameprev = tk.Label(self.main_frame, font=("Courier", 10, "italic"), fg="#666", bg="#f5f5f5")
+        self.lbl_expfile_nameprev.grid(row=14, column=0, columnspan=4, sticky="w", pady=(5, 0))
+
+        ## --- Export Button ---
+        self.btn_export = tk.Button(self.main_frame, command=self.export_start, bg="#4caf50", fg="white", font=("Arial", 12, "bold"), height=2)
+        self.btn_export.grid(row=15, column=0, columnspan=4, sticky="ew", pady=30)
+
+        ## --- Links ---
+        ### Link to Repo
+        self.link_repo = tk.Label(self.main_frame, fg="#0056b3", bg="#f5f5f5", cursor="hand2", font=("Arial", 8, "underline"))
+        self.link_repo.grid(row=16, column=0, sticky="w")
+        self.link_repo.bind("<Button-1>", lambda a: webbrowser.open_new("https://github.com/MikeCat2008/smartboard-notebook-exporter"))
+
+        ### Link to License
+        self.link_license = tk.Label(self.main_frame, fg="#0056b3", bg="#f5f5f5", cursor="hand2", font=("Arial", 8, "underline"))
+        self.link_license.grid(row=17, column=0, sticky="w")
+        self.link_license.bind("<Button-1>", lambda a: webbrowser.open_new("https://www.gnu.org/licenses/gpl-3.0.html"))
+
+        #self.test = tk.Label(root, font=("Arial", 14))
+        #self.test.pack()
+
+        self.update_texts()
+
+    # --- FUNCIONS ---
+    def nbfile_search(self):
+        filename = filedialog.askopenfilename(filetypes=[("Smartboard notebook","*.notebook")])
+        if filename:
+            self.ent_nbfile.delete(0, tk.END)
+            self.ent_nbfile.insert(0, filename)
+
+    def expfile_outfold_search(self):
+        directory = filedialog.askdirectory()
+        if directory:
+            self.ent_expfile_outdir.delete(0, tk.END)
+            self.ent_expfile_outdir.insert(0, directory)
+
+    def expfile_nameprev_update(self, *args):
+        if self.var_expfile_name.get():
+            name = self.var_expfile_name.get()
+        elif self.var_nbfile.get():
+            name = Path(self.var_nbfile.get()).stem
+        else:
+            name = "my_file"
+        outtype = self.var_expfile_outtype.get()
+        outtype_ext_map = {
             'svg-fixed-pages': 'zip',
             'png-pages': 'zip',
             'pdf-svg-pages': 'zip',
@@ -36,130 +185,96 @@ class ExporterApp:
             'pdf-svg-merged': 'pdf',
             'pdf-png-merged': 'pdf'
         }
-        self.full_name = None
-        
-        # ----- MAIN FRAME -----
-        self.main_frame = tk.Frame(root, bg="#f5f5f5", padx=20, pady=20) # Margin for all the window
-        self.main_frame.pack()
-
-        # --- REACTIVE VARIABLES ---
-        self.var_in_name = tk.StringVar(value="")
-        self.var_out_name = tk.StringVar(value="")
-        self.var_type = tk.StringVar()
-        self.var_ext_toggle = tk.BooleanVar(value=True)
-
-        # Search for changes for name preview
-        self.var_out_name.trace_add("write", self.update_preview)
-        self.var_in_name.trace_add("write", self.update_preview)
-        self.var_type.trace_add("write", self.update_preview)
-        self.var_ext_toggle.trace_add("write", self.update_preview)
-
-        # --- TITLE ---
-        tk.Label(self.main_frame, text="Smartboard notebook Exporter", font=("Arial", 20, "bold"), bg="#f5f5f5", fg="#333").grid(row=0, column=0, columnspan=4, pady=(0, 10))
-        
-        ttk.Separator(self.main_frame, orient='horizontal').grid(row=1, column=0, columnspan=4, sticky="ew", pady=(0, 20))
-
-        # --- NOTEBOOK FILE ---
-        tk.Label(self.main_frame, text="Notebook File", font=("Arial", 14, "bold"), bg="#f5f5f5").grid(row=2, column=0, sticky="w")
-        
-        self.ent_notebook = tk.Entry(self.main_frame, textvariable=self.var_in_name, width=40)
-        self.ent_notebook.grid(row=3, column=0, columnspan=3, sticky="ew", padx=(0, 10), pady=10)
-        
-        tk.Button(self.main_frame, text="BROWSE", command=self.search_file).grid(row=3, column=3, sticky="ew")
-
-        # --- EXPORT FILE ---
-        tk.Label(self.main_frame, text="Export File", font=("Arial", 14, "bold"), bg="#f5f5f5").grid(row=4, column=0, sticky="w", pady=(20, 5))
-        
-        # Output Folder
-        tk.Label(self.main_frame, text="Output Folder", font=("Arial", 10), bg="#f5f5f5").grid(row=5, column=0, sticky="w")
-        self.ent_output = tk.Entry(self.main_frame)
-        self.ent_output.grid(row=6, column=0, columnspan=3, sticky="ew", padx=(0, 10), pady=5)
-        tk.Button(self.main_frame, text="BROWSE", command=self.search_dir).grid(row=6, column=3, sticky="ew")
-
-        # Name and Type
-        tk.Label(self.main_frame, text="Name (opt)", font=("Arial", 10), bg="#f5f5f5").grid(row=7, column=0, sticky="w", pady=(10, 0))
-        tk.Label(self.main_frame, text="Output Type", font=("Arial", 10), bg="#f5f5f5").grid(row=7, column=1, sticky="w", pady=(10, 0))
-        
-        self.ent_name = tk.Entry(self.main_frame, textvariable=self.var_out_name)
-        self.ent_name.grid(row=8, column=0, sticky="ew", padx=(0, 5))
-        
-        self.combo_type = ttk.Combobox(self.main_frame, state="readonly", textvariable=self.var_type, values=['svg-fixed-pages', 'png-pages', 'pdf-svg-pages', 'pdf-png-pages', 'pdf-svg-merged', 'pdf-png-merged'])
-        self.combo_type.current(0)
-        self.combo_type.grid(row=8, column=1, columnspan=2, sticky="ew", padx=5)
-        
-        tk.Checkbutton(self.main_frame, variable=self.var_ext_toggle, text="Extension Name Toggle", bg="#f5f5f5").grid(row=8, column=3)
-
-        # --- PREVIEW LABEL ---
-        self.lbl_preview = tk.Label(self.main_frame, text="", font=("Courier", 10, "italic"), fg="#666", bg="#f5f5f5")
-        self.lbl_preview.grid(row=9, column=0, columnspan=4, sticky="w", pady=(5, 0))
-
-        self.update_preview()
-
-        # --- EXPORT ---
-        self.btn_export = tk.Button(self.main_frame, text="EXPORT", command=self.export, bg="#4CAF50", fg="white", font=("Arial", 12, "bold"), height=2)
-        self.btn_export.grid(row=10, column=0, columnspan=4, sticky="ew", pady=30)
-
-        # --- LINKS ---
-        # Link to Repo (Left)
-        link_repo = tk.Label(self.main_frame, text="GitHub Repository", fg="#0056b3", bg="#f5f5f5", cursor="hand2", font=("Arial", 8, "underline"))
-        link_repo.grid(row=11, column=0, sticky="w")
-        link_repo.bind("<Button-1>", lambda a: webbrowser.open_new("https://github.com/MikeCat2008/smartboard-notebook-exporter"))
-
-        # Link to License (Right)
-        link_license = tk.Label(self.main_frame, text="GPLv3 License", fg="#0056b3", bg="#f5f5f5", cursor="hand2", font=("Arial", 8, "underline"))
-        link_license.grid(row=12, column=0, sticky="w")
-        link_license.bind("<Button-1>", lambda a: webbrowser.open_new("https://www.gnu.org/licenses/gpl-3.0.html"))
-
-    def search_file(self):
-        filename = filedialog.askopenfilename(filetypes=[("Smartboard notebook","*.notebook")])
-        if filename:
-            self.ent_notebook.delete(0, tk.END)
-            self.ent_notebook.insert(0, filename)
-
-    def search_dir(self):
-        directory = filedialog.askdirectory()
-        if directory:
-            self.ent_output.delete(0, tk.END)
-            self.ent_output.insert(0, directory)
-
-    def update_preview(self, *args):
-        if self.var_out_name.get():
-            name = self.var_out_name.get()
+        ext = outtype_ext_map.get(outtype, "ext")
+        if self.var_expfile_outtypeext.get():
+            self.var_expfile_nameprev = f"{name}_{outtype}.{ext}"
         else:
-            if self.var_in_name.get():
-                name = Path(self.var_in_name.get()).stem
-            else:
-                name = "my_file"
-        out_type = self.var_type.get()
-        ext = self.extension_map[out_type]
+            self.var_expfile_nameprev = f"{name}.{ext}"
+        self.update_texts()
 
-        if self.var_ext_toggle.get():
-            # Long format: file_pdf-svg-merged.pdf
-            self.full_name = f"{name}_{out_type}.{ext}"
-        else:
-            # Short format: file.pdf
-            self.full_name = f"{name}.{ext}"
+    ## --- Export ---
+    def export_enable(self):
+        self.var_export_active.set(False)
+        self.btn_export.config(bg="#4caf50", state="normal")
+        self.update_texts()
 
-        self.lbl_preview.config(text=f"Preview: {self.full_name}")
+    def export_start(self):
+        self.var_export_active.set(True)
+        self.btn_export.config(bg="#af4c50", state="disabled")
+        self.update_texts()
 
-    def export(self):
-        infile = self.var_in_name.get().strip()
-        export_type = self.var_type.get()
-        export_path = self.ent_output.get().strip()  # If empty ""
-        export_name = self.var_out_name.get().strip() # If empty: ""
-        otnextension_bool = self.var_ext_toggle.get()
+        # 50ms for UI Rendering before export_process
+        self.btn_export.after(50, self.export_process)
+
+    def export_process(self):
+        infile = self.var_nbfile.get().strip()
+        export_type = self.var_expfile_outtype.get()
+        export_path = self.var_expfile_outdir.get().strip()  # If empty ""
+        export_name = self.var_expfile_name.get().strip() # If empty: ""
+        otnextension_bool = self.var_expfile_outtypeext.get()
 
         if not infile:
-            tk.messagebox.showwarning("Remaining File", "Please, select a .notebook file first.")
+            tk.messagebox.Message(
+                icon="warning",
+                type="ok",
+                message=self.gettext("msgbox_warn_missfile_msg"),
+                detail=self.gettext("msgbox_warn_missfile_dtl")
+            ).show()
+            self.export_enable()
             return
 
         try:
             main.main(infile, export_type, export_path, export_name, otnextension_bool)
-            tk.messagebox.showinfo("Success!", f"Your file '{self.full_name}' has been successfuly exported.")
+            tk.messagebox.Message(
+                icon="info",
+                type="ok",
+                message=self.gettext("msgbox_info_ok_msg"),
+                detail=self.gettext("msgbox_info_ok_dtl")
+            ).show()
+            self.export_enable()
         except Exception as e:
-            tk.messagebox.showwarning("Error",f"Something went wrong:\n{e}")
+            tk.messagebox.Message(
+                icon="error",
+                type="ok",
+                message=self.gettext("msgbox_err_exp_msg"),
+                detail=f"{self.gettext("msgbox_err_exp_dtl")}\n{e}"
+            ).show()
+            self.export_enable()
+
+    ## --- i18n ---
+    def set_lang(self, langkey):
+        self.var_lang = langkey
+        self.update_texts()
+
+    def gettext(self, textkey):
+        return gui_locales.locales[self.var_lang].get(textkey, textkey)
+
+    def update_texts(self):
+        self.lbl_ver.config(text=self.gettext("lbl_ver"))
+        self.lbl_ver_curtxt.config(text=self.gettext("lbl_ver_curtxt"))
+        self.btn_ver_chkupd.config(text=self.gettext("btn_ver_chkupd"))
+        self.lbl_lang.config(text=self.gettext("lbl_lang"))
+        self.lbl_lang_info.config(text=self.gettext("lbl_lang_info"))
+        self.lbl_nbfile.config(text=self.gettext("lbl_nbfile"))
+        self.btn_nbfile_browse.config(text=self.gettext("btn_nbfile_browse"))
+        self.lbl_expfile.config(text=self.gettext("lbl_expfile"))
+        self.lbl_expfile_outdir.config(text=self.gettext("lbl_expfile_outdir"))
+        self.btn_expfile_outdir.config(text=self.gettext("btn_expfile_outdir"))
+        self.lbl_expfile_name.config(text=self.gettext("lbl_expfile_name"))
+        self.lbl_expfile_outtype.config(text=self.gettext("lbl_expfile_outtype"))
+        self.chkbtn_expfile_outtypeext.config(text=self.gettext("chkbtn_expfile_outtypeext"))
+        self.lbl_expfile_nameprev.config(text=f"{self.gettext("lbl_expfile_nameprev")} {self.var_expfile_nameprev}")
+        if self.var_export_active.get():
+            self.btn_export.config(text=self.gettext("btn_export_active"))
+        else:
+            self.btn_export.config(text=self.gettext("btn_export"))
+        self.link_repo.config(text=self.gettext("link_repo"))
+        self.link_license.config(text=self.gettext("link_license"))
+
+        #self.test.config(text=self.gettext("test"))
 
 if __name__ == "__main__":
     root = tk.Tk()
+    root.geometry("650x650")
     app = ExporterApp(root)
     root.mainloop()
---
+++ src/gui_locales.py
@@ -0,0 +1,74 @@
+# smartboard-notebook-exporter
+# Copyright (C) 2026  MikeCat2008
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+locales = {
+    "es": {
+        "lbl_ver": "Versión",
+        "lbl_ver_curtxt": "Actual:",
+        "btn_ver_chkupd": "Comprobar Actualizaciones",
+        "lbl_lang": "Idioma",
+        "lbl_lang_info": "Español (Spanish)",
+        "lbl_nbfile": "Archivo Notebook",
+        "btn_nbfile_browse": "BUSCAR",
+        "lbl_expfile": "Exportar Archivo",
+        "lbl_expfile_outdir": "Carpeta de Salida (Opcional)",
+        "btn_expfile_outdir": "BUSCAR",
+        "lbl_expfile_name": "Nombre (Opcional)",
+        "lbl_expfile_outtype": "Tipo de Conversión",
+        "chkbtn_expfile_outtypeext": "Añadir Etiqueta de Conversión",
+        "lbl_expfile_nameprev": "Previsualización del Nombre:",
+        "btn_export": "EXPORTAR",
+        "btn_export_active": "EXPORTANDO...",
+        "msgbox_warn_missfile_msg": "Archivo Faltante",
+        "msgbox_warn_missfile_dtl": "Porfavor, selecciona un archivo .notebook antes.",
+        "msgbox_info_ok_msg": "Éxito!",
+        "msgbox_info_ok_dtl": "Su archivo ha sido exportado existosamente.",
+        "msgbox_err_exp_msg": "Error durante la Exportación",
+        "msgbox_err_exp_dtl": "Algo fue mal:",
+        "link_repo": "Repositorio de GitHub",
+        "link_license": "Licencia GPLv3",
+
+        "test": "Me gustan los furros :3" # nadie lo sabrá >w< - MikeCat2008
+    },
+    "en": {
+        "lbl_ver": "Version",
+        "lbl_ver_curtxt": "Current:",
+        "btn_ver_chkupd": "Check Updates",
+        "lbl_lang": "Language",
+        "lbl_lang_info": "English (English)",
+        "lbl_nbfile": "Notebook File",
+        "btn_nbfile_browse": "BROWSE",
+        "lbl_expfile": "Export File",
+        "lbl_expfile_outdir": "Output Folder (Optional)",
+        "btn_expfile_outdir": "BROWSE",
+        "lbl_expfile_name": "Name (Optional)",
+        "lbl_expfile_outtype": "Output Type",
+        "chkbtn_expfile_outtypeext": "Extension Name Toggle",
+        "lbl_expfile_nameprev": "Name Preview:",
+        "btn_export": "EXPORT",
+        "btn_export_active": "EXPORTING...",
+        "msgbox_warn_missfile_msg": "Missing File",
+        "msgbox_warn_missfile_dtl": "Please, select a .notebook file first.",
+        "msgbox_info_ok_msg": "Success!",
+        "msgbox_info_ok_dtl": "Your file has been successfuly exported",
+        "msgbox_err_exp_msg": "Error while Exporting",
+        "msgbox_err_exp_dtl": "Something went wrong:",
+        "link_repo": "GitHub Repository",
+        "link_license": "GPLv3 License",
+
+        "test": "I like furries :3" # nobody's gonna know >w< - MikeCat2008
+    }
+}
