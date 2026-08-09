4f4b4a8ee2157807aad64917e6bb058d1dafda58
MikeCat2008 <samuelgargri@gmail.com>
2026-08-10 00:36:55 +0200

    build: update version in output binary name

--- build.bat
+++ build.bat
@@ -1,1 +1,1 @@
-pyinstaller --onefile --windowed --collect-all cairosvg --name "sbne-v1.0.0-windows" src\gui.py
+pyinstaller --onefile --windowed --collect-all cairosvg --name "sbne-v1.1.0-windows" src\gui.py
