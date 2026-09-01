1d46cd1714193a7e9cec483e0ae8e2b48cab41f4
CommandoMagia <iago.alleres@gmail.com>
2026-09-01 18:22:44 +0200

    fix: minor patches ;3

--- build.bat
+++
@@ -1,1 +0,0 @@
-pyinstaller --onefile --windowed --collect-all cairosvg --name "sbne-v1.3.0-windows" src\gui.py
--- build.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-pyinstaller --onefile --windowed --collect-all cairosvg --name "sbne-v1.3.0-linux" src/gui.py
